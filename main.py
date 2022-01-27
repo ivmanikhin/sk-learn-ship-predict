5edbcbcbe3dad5a6ec9c39f73273d0032864c1f5
ivmanikhin <ivmanikhin@gmail.com>
2022-01-27 19:17:21 +0300

    Fist attempt. Not the best :)

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import pandas as pd
 from tabulate import tabulate
 from easy_SQL.easy_SQL import *
 from scipy import stats
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression, LinearRegression, SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostRegressor, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 
 SEP = os.sep
 
@@ -13,35 +19,56 @@ pd.set_option("display.max_rows", None)
 pd.set_option('max_colwidth', None)
 
 
-dataset = extract_table_from_sql(table_name="main_ships_data_raw",
-                                 sql_column_names="type, loa, boa, draft, speed, power, year",
-                                 df_column_names=["type", "loa", "boa", "draft", "speed", "power", "year"],
-                                 additional_parameters="where year > 1985 and loa > 30 and boa not NULL and (loa / boa) > 2 and draft between 1 and 30 and speed between 5 and 100 and power between 10 and 80000")
-# print(tabulate(dataset.head(50), headers='keys', tablefmt='psql'))
-# dataset = dataset.sample(frac=0.1)
-print(dataset.shape)
-dataset["loa"] = dataset["loa"].astype("float64")
-dataset["boa"] = dataset["boa"].astype("float64")
-dataset["draft"] = dataset["draft"].astype("float64")
-dataset["power"] = dataset["power"].astype("float64")
-dataset["speed"] = dataset["speed"].astype("float64")
-dataset["year"] = dataset["year"].astype("int32")
-dataset["vol"] = dataset["loa"] * dataset["boa"] * dataset["draft"]
-dataset["fatness"] = dataset["loa"] / dataset["boa"]
-good_rows = (np.abs(stats.zscore(dataset["fatness"])) < 3)
-dataset = dataset[good_rows]
-print(dataset.shape)
+# Getting data from SQLite
+df = extract_table_from_sql(table_name="main_ships_data_raw",
+                            sql_column_names="type, loa, boa, draft, speed, power, year",
+                            df_column_names=["type", "loa", "boa", "draft", "speed", "power", "year"],
+                            additional_parameters="where year > 1985 and loa > 30 and boa not NULL and (loa / boa) > 2 and draft between 1 and 30 and speed between 5 and 100 and power between 10 and 80000")
+# print(tabulate(df.head(50), headers='keys', tablefmt='psql'))
+# df = df.sample(frac=0.1)
+print(df.shape)
+
+# Converting to numbers
+df["loa"] = df["loa"].astype("float64")
+df["boa"] = df["boa"].astype("float64")
+df["draft"] = df["draft"].astype("float64")
+df["power"] = df["power"].astype("float64")
+df["speed"] = df["speed"].astype("float64")
+df["year"] = df["year"].astype("int32")
+
+# Removing outliers
+df["fatness"] = df["loa"] / df["boa"]
+good_rows = (np.abs(stats.zscore(df["fatness"])) < 3)
+df = df[good_rows]
+print(df.shape)
 
 
+# making dataset
+y = df.pop("power").values
+X = df[["loa", "boa", "draft", "speed"]].values.tolist()
 
 
-# plt.rcParams["figure.figsize"] = (8,6)
-# ax = plt.axes(projection='3d')
-# ax.scatter(dataset["vol"], dataset["speed"], dataset["power"], s=3, c=(dataset["fatness"]))
-# ax.set_xlabel("vol")
-# ax.set_ylabel("speed")
-# ax.set_zlabel("power")
-# plt.show()
+# splitting to train and test datasets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+pred = lr.predict(X_test)
+
+
+plt.rcParams["figure.figsize"] = (8,6)
+ax = plt.axes(projection='3d')
+vol = [a[0] * a[1] * a[2] for a in X_test]
+speed = [a[3] for a in X_test]
+print(vol)
+
+ax.scatter(vol, speed, y_test, s=3, c="b")
+ax.scatter(vol, speed, pred, s=3, c="r")
+ax.set_xlabel("vol")
+ax.set_ylabel("speed")
+ax.set_zlabel("power")
+plt.show()
 
 
 

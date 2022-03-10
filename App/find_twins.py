1e26436227d7a3ecaab278cb58a1dc7b2bbb84de
ivmanikhin <ivmanikhin@gmail.com>
2022-03-10 23:37:12 +0300

    Now it works at AWS

--- App/find_twins.py
+++ App/find_twins.py
@@ -1,10 +1,10 @@
-import mariadb
+import pymysql
 from App.constants import *
 
 
 def extract_table_from_sql(where="imo_no in (9832717, 9864332)"):
     try:
-        with mariadb.connect(host=HOST, user=USER, password=PASSWORD, port=PORT, database=DATABASE) as con:
+        with pymysql.connect(host=HOST, user=USER, password=PASSWORD, port=PORT, database=DATABASE) as con:
             cur = con.cursor()
             cur.execute(f"SELECT {COL_NAMES} FROM {TABLE_NAME} WHERE {where}")
             raw_data = cur.fetchall()
